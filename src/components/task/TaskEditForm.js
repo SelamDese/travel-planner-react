e398276c68a12fb8d7cfaeaa3535c9c377009b4b
selam <selam0315@gmail.com>
2019-02-07 13:56:32 -0600

    task edit form

---
+++ src/components/task/TaskEditForm.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react"
+import TaskManager from "../../modules/TaskManager"
+
+export default class TaskEditForm extends Component {
+
+    state = {
+        taskName: "",
+        dueDate: "",
+        complitionDate: ""
+       }
+
+    handleEditTaskFieldChange = evt => {
+        const stateToChange = {}
+        stateToChange[evt.target.id] = evt.target.value
+        this.setState(stateToChange)
+    }
+
+    updateExistingTask = evt => {
+      evt.preventDefault()
+      const existingTask = {
+        taskName: this.state.taskName,
+        dueDate: this.state.dueDate,
+        complitionDate: this.state.complitionDate
+      }
+
+    this.props.updateTask(this.props.match.params.taskId, existingTask)
+    .then(() => this.props.history.push("/tasks"))      
+    }
+
+    componentDidMount() {
+      TaskManager.get(this.props.match.params.taskId)
+      .then(tasks => {
+        this.setState({
+            taskName: tasks.taskName,
+            dueDate: tasks.dueDate,
+            complitionDate: tasks.complitionDate
+        });
+      });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <form>
+                    <div>
+                        <label htmlFor="taskName">Task Name</label>
+                        <input type="text" required id="taskName" value = {this.state.taskName}
+                          onChange={this.handleEditTaskFieldChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="dueDate">Due Date</label>
+                        <input type="date" required id="dueDate" value={this.state.dueDate}
+                          onChange={this.handleEditTaskFieldChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="complitionDate">Complition Date</label>
+                        <input type="date" required id="complitionDate" value={this.state.complitionDate}
+                          onChange={this.handleEditTaskFieldChange}
+                           />
+                    </div>
+                    
+                    <button type="submit" onClick={this.updateExistingTask} >Submit</button>
+                </form>
+            </React.Fragment>
+        )
+    }
+}
