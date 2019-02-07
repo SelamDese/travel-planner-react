21d51630f0495070de9662bf9d6c7dd9a31f4e21
selam <selam0315@gmail.com>
2019-02-07 13:33:47 -0600

    add and post new task

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,17 +1,18 @@
 import { Route, Redirect } from "react-router-dom"
 import React, { Component } from "react"
 import TravelerList from "./traveler/TravelerList"
-// import TaskList from "./task/TaskList"
+import TaskList from "./task/TaskList"
 // import TripList from "./trip/TripList"
 import LoginList from "./login/LoginList"
 import Registration from "./login/Registration"
 import TravelerForm from "./traveler/TravelForm"
 // import TripForm from "./trip/TripForm"
-// import TaskForm from "./task/TaskForm"
+import TaskForm from "./task/TaskForm"
+import TaskEditForm from "./task/TaskEditForm"
 import LoginForm from "./login/LoginForm"
 import RegistrationForm from "./login/RegistrationForm"
 import TravelerManager from "../modules/TravelerManager"
-// import TaskManager from "../modules/TaskManager"
+import TaskManager from "../modules/TaskManager"
 // import TripManager from "../modules/TripManager"
 import LoginManager from "../modules/LoginManager"
 
@@ -31,17 +32,26 @@ export default class ApplicationViews extends Component {
           })
         )
         }
+     addNewTask = (task) =>{
+      return TaskManager.post(task)
+        .then(() => TaskManager.getAll())
+        .then(tasks =>
+          this.setState({
+            tasks: tasks
+          })
+        )
+        } 
 
-        registerNewUser(users){
-            return LoginManager.post(users)
-              //   .then(() => {
-              //   return LoginManager.getAll()})
-              //   .then(users =>
-              //   this.setState({
-              //   users: users
-              //   })
-              // )
-            }
+       registerNewUser(users){
+          return LoginManager.post(users)
+            //   .then(() => {
+            //   return LoginManager.getAll()})
+            //   .then(users =>
+            //   this.setState({
+            //   users: users
+            //   })
+            // )
+          }
 
      componentDidMount() {
 
@@ -51,11 +61,17 @@ export default class ApplicationViews extends Component {
           })
         })
 
-          LoginManager.getAll().then(allUsers => {
-            this.setState({
-                users: allUsers
-            })
+        TaskManager.getAll().then(allTasks => {
+          this.setState({
+              tasks: allTasks
           })
+        })
+
+        LoginManager.getAll().then(allUsers => {
+          this.setState({
+              users: allUsers
+          })
+        })
     }
 
     render() {
@@ -90,6 +106,16 @@ export default class ApplicationViews extends Component {
             <Route
               exact path="/newTraveler" render={props => {
                 return ( <TravelerForm {...props} addNewTraveler={this.addNewTraveler} /> )
+              }}
+            />
+            <Route
+              path="/tasks" render={props => {
+                return <TaskList {...props} {...props} deleteTask={this.deleteTask} tasks={this.state.tasks}/>
+              }}
+            />
+            <Route
+              path="/newTask" render={props => {
+                return <TaskForm {...props} addNewTask={this.addNewTask}/>
               }}
             />
         </React.Fragment>
